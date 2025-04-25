372c857956bf56a4514e81495a035d0eeda2af55
melissa0987 <1468444@edu.vaniercollege.qc.ca>
2025-04-24 23:10:47 -0400

    added necessary files, started css

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# portfolio
+[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/NPKL5qNZ)
+# Front End Web Programming Project
+
+420-931-VA - Front End Web Programming Project (Winter 2025)
+
+This template repository is the starter project for Front End Web Programming Project. Written in HTML, CSS and JavaScript.
+
+### Project Description
+
+You will create a personal website for your project using HTML5, CSS3, Bootstrap, and Javascript. This is an individual project.
+
+There should be the following pages/elements on your website:
+
+- Homepage (5%)
+- Contact page (5%)
+- About page (5%)
+- My work page (Portfolio page) (5%)
+- Services page (if you like to do freelance work) OR Blog page (5%)
+- Thoughtful placement of the elements of the page e.g. buttons, menus, etc, and having links to different social media platforms e.g. LinkedIn (5%)
+- Proper use of fonts, colors, and overall design (5%)
+- Making the pages dynamic using JavaScript (5%)
+
+The repository should be uploaded on GitHub and there should be frequent commits as the project is being developed. **There will be a 5% grade loss if these criteria are not fulfilled.** 
---
+++ js/script.js
@@ -0,0 +1,157 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  // footer current yr
+  const currentYR = document.getElementById('currentYR');
+  var currentYear = new Date().getFullYear();
+  currentYR.textContent = currentYear;
+
+  const themeToggle = document.getElementById("theme-toggle");
+  const themeIcon = document.getElementById("theme-icon");
+
+  // Set light mode as default
+  document.body.classList.remove("dark-mode");
+  themeIcon.classList.add("bi-sun"); // Sun icon for light mode
+
+  // Toggle light/dark mode
+  themeToggle.addEventListener("click", function () {
+    if (document.body.classList.contains("dark-mode")) {
+      document.body.classList.remove("dark-mode");
+      themeIcon.classList.remove("bi-moon"); //removes moon icon
+      themeIcon.classList.add("bi-sun"); //adds sun icon
+      console.log("Switched to light mode");
+    } else {
+      document.body.classList.add("dark-mode");
+      themeIcon.classList.remove("bi-sun"); //removes sun icon
+      themeIcon.classList.add("bi-moon"); //adds moon icon
+      console.log("Switched to dark mode");
+    }
+  });
+
+  // funtion for arrow to the top
+  const backToTopButton = document.getElementById("arrowTop");
+
+  window.onscroll = function () {
+    if (
+      document.body.scrollTop > 50 ||
+      document.documentElement.scrollTop > 50
+    ) {
+      backToTopButton.classList.add("show");
+    } else {
+      backToTopButton.classList.remove("show");
+    }
+  };
+
+  //hover on the project logos
+  const projContainers = document.getElementsByClassName("proj-cont"); 
+  Array.from(projContainers).forEach((container, index) => {
+    //hovering on 
+    container.addEventListener("mouseover", function () {
+      let pDecID = projContainers[index].children[1].children[1];
+      let pText = "";
+
+      // Course Manager Web App
+      if (pDecID == container.querySelector("#comp-desc")) {
+        pText = "Built with Python, Flask, Oracle, and REST API," +
+            " this app manages users and displays course" + 
+            " competencies. It features secure login," + 
+            " data visualization, and CRUD functionalities," + 
+            " designed for scalability and smooth user interaction."
+      }
+      // record store
+      if (pDecID == container.querySelector("#record-desc")) {
+        pText = "Created a record store app using Java," + 
+            " JavaFX, and SQL. Managed records by artist, " + 
+            " title, genre, and availability, while" + 
+            " optimizing database queries for performance" + 
+            " and testing features with JUnit."
+      }
+
+      // Course Manager Web App
+      if (pDecID == container.querySelector("#tindog-desc")) {
+        pText = "A responsive web app built with HTML5," + 
+          " CSS, and Bootstrap that helps dog owners " +
+          " connect their pets with nearby companions in a " +
+          " fun, user-friendly interface."
+      }
+
+      // Course list db
+      if (pDecID == container.querySelector("#courselist-desc")) {
+        pText = "A SQL and Java-based system for tracking" + 
+            " computer science courses and prerequisites," + 
+            " featuring advanced SQL techniques(views," + 
+            " triggers, procedures) and a Java app for" + 
+            " efficient data management."
+      }
+      if (pDecID == container.querySelector("#movie-desc")) {
+        pText = "Using PostgreSQL to develop a movie rental system designed" +  
+            " to manage memberships, rentals, and movie" +  
+            " inventory. The system ensures proper handling" +  
+            " of rental transactions, membership benefits," +  
+            " and overdue penalties."
+      }
+
+      if (pDecID == container.querySelector("#memFee-desc")) {
+        pText = "Windows Forms application in C#" +  
+              " that allows users to enter personal details," + 
+              " select membership types, choose additional options," + 
+              " calculate fees, and save membership details to a CSV " +
+              " file, with the ability to view registered memberships " +
+              " in a separate form"
+      }
+
+      if (pDecID == container.querySelector("#exchange-desc")) {
+        pText = "Developed using HTML, CSS, and JavaScript, this" +  
+              " project retrieves and displays real-time data " + 
+              " from an external API"
+      }
+
+      pDecID.textContent = pText;
+      pDecID.style.fontSize = "15px";
+    });
+
+    container.addEventListener("mouseout", function () {
+      pDecID = projContainers[index].children[1].children[1];
+      // <!-- Course Manager Web App -->
+      if (pDecID == container.querySelector("#comp-desc")) {
+        pDecID.textContent = "";
+      }
+      // <!-- CRecord Store Appp -->
+      if (pDecID == container.querySelector("#record-desc")) {
+        pDecID.textContent = "";
+      }
+      // <!-- Tindog Website -->
+      if (pDecID == container.querySelector("#tindog-desc")) {
+        pDecID.textContent = "";
+      }
+      // <!--Course List Database -->
+      if (pDecID == container.querySelector("#courselist-db")) {
+        pDecID.textContent = "";
+      }
+      // <!--Movie Rental Database -->
+      if (pDecID == container.querySelector("#movie-desc")) {
+        pDecID.textContent = "";
+      }
+      // <!-- Projects Membership Fee Calculator -->
+      if (pDecID == container.querySelector("#memFee-desc")) {
+        pDecID.textContent = "";
+      }
+      // Exchange Rate Calculator
+      if (pDecID == container.querySelector("#exchange-db")) {
+        pDecID.textContent = "";
+      }
+    });
+  });
+
+
+  // change photo when hover
+  const portphoto = document.getElementById("port-photo");
+  portphoto.addEventListener("mouseenter", (event) =>{
+    event.target.src = "images/port-photo.png";
+    event.target.alt = "portfolio-person"
+  });
+  portphoto.addEventListener("mouseleave", (event) =>{
+    event.target.src = "images/portforlio.png";
+    event.target.alt = "portfolio-ghibli"
+  });
+
+});
